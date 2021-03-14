f2b65a9b97ddd743f53670048f96ab2c053069cc
Tyler Samples <samples.tm@gmail.com>
2021-03-14 15:06:34 -0600

    Use esm deps

--- mix.exs
+++ mix.exs
@@ -33,11 +33,11 @@ defmodule Demo.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.5.8"},
+      {:phoenix, git: "https://github.com/phoenixframework/phoenix", override: true},
       {:phoenix_ecto, "~> 4.1"},
       {:ecto_sql, "~> 3.4"},
       {:postgrex, ">= 0.0.0"},
-      {:phoenix_live_view, "~> 0.15.1"},
+      {:phoenix_live_view, git: "https://github.com/tylersamples/phoenix_live_view", branch: "esm-exports", override: true},
       {:floki, ">= 0.27.0", only: :test},
       {:phoenix_html, "~> 2.11"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
